c501350bc5c51dc5c13752a9c0c322e26f3c2ad2
Bitate <1660513875@qq.com>
2020-12-20 21:14:22 +0800

    Daily Commit

--- include/MainWindow.hpp
+++ include/MainWindow.hpp
@@ -3,7 +3,12 @@
 #include <QMainWindow>
 #include <QFileDialog>
 #include <QPushButton>
+#include <QAbstractItemModel>
 #include <QListView>
+#include <QStyledItemDelegate>
+
+// forward declaration
+class AnkiDeckNamesModel;
 
 class MainWindow : public QMainWindow
 {
@@ -17,4 +22,17 @@ private:
     QFileDialog* file_dialog;
     QPushButton* open_file_push_button;
     QListView* deck_names_tree_view;
+    QStyledItemDelegate* styled_item_delegate;
+
+};
+
+class AnkiDeckNamesModel : public QAbstractItemModel
+{
+    Q_OBJECT
+public:
+    AnkiDeckNamesModel(QObject* parent = nullptr);
+
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+
+
 };
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,18 +2,18 @@
 
 MainWindow::MainWindow()
     : file_dialog(new QFileDialog()), open_file_push_button(new QPushButton()),
-      deck_names_tree_view(new QListView())
+      deck_names_tree_view(new QListView()), styled_item_delegate(new QStyledItemDelegate())
 {
 
 }
 
 void MainWindow::show_main_window()
 {
-
-
     setCentralWidget(deck_names_tree_view);
 
     setWindowTitle("Anker");
+
+    // styled_item_delegate->setModelData();
 
     show();
 }
